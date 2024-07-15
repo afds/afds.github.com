4038a015f660641c6247bb880cc8535bbdd55874
Deniss Afonin <deniss.afonin@gmail.com>
2024-07-15 18:44:59 +0300

    update

--- race-viewer/flutter_bootstrap.js
+++ race-viewer/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3810411247"
+    serviceWorkerVersion: "227722051"
   }
 });
